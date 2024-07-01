80d310b7b64f4bc5df734583005a606070e77e2d
alex.wang@therealreal.com <alex.wang@therealreal.com>
2024-07-01 10:24:56 -0500

    first commit

---
+++ lib/example.ex
@@ -0,0 +1,28 @@
+defmodule Example do
+  use Application
+
+  # @x 5
+  # alias UUID
+  def start(_type, _args) do
+    Example.main()
+    Supervisor.start_link([], strategy: :one_for_one)
+  end
+
+  def main do
+    # name = "test";
+    # status = Enum.random([:gold, :silver, :bronze])
+
+    # case status do
+    #   :gold -> IO.puts("Welcome to the lounge, #{name}")
+    #   :bronze -> IO.puts("not part of team")
+    #   _-> IO.puts("Out of bounds")
+    # IO.puts(?a)
+    a = 10
+    b = 3
+    IO.puts(Integer.mod(b, a))
+    :io.format("~.20f\n", [0.1])
+    memberships = {:bronze, :silver}
+    memberships = Tuple.append(memberships, :gold)
+    IO.inspect(memberships)
+    end
+end
